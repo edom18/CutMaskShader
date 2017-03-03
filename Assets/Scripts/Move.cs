b7e9b2ab30cd65c8c6edfe50c5779bae24358e50
edom18 <edo.m18@gmail.com>
2017-03-03 12:44:36 +0900

    Added move script for demo

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float _frecency = 2f;
+
+    [SerializeField]
+    private float _speed = 2f;
+
+    private Vector3 _origin;
+
+	void Start()
+    {
+        _origin = transform.position;
+	}
+	
+	void Update()
+    {
+        float z = Mathf.Sin(Time.time * _speed) * _frecency;
+        transform.position = _origin + new Vector3(0, 0, z);
+	}
+}
